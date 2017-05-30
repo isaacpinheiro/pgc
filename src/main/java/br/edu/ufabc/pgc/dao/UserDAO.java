ce9ea98fee88292010baba109bfb97fe81cdf856
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-05-30 11:45:32 -0300

    Implementando o UserDAO.

--- src/main/java/br/edu/ufabc/pgc/dao/UserDAO.java
+++ src/main/java/br/edu/ufabc/pgc/dao/UserDAO.java
@@ -9,6 +9,71 @@ package br.edu.ufabc.pgc.dao;
  *
  * @author isaac
  */
+
+import java.util.List;
+import javax.persistence.EntityManager;
+
+import br.edu.ufabc.pgc.util.EntityManagerUtil;
+import br.edu.ufabc.pgc.model.User;
+
 public class UserDAO {
+    
+    private final EntityManager em;
+    
+    public UserDAO() {
+        this.em = EntityManagerUtil.getEntityManager();
+    }
+    
+    public User find(Long id) {
+        
+        User obj = null;
+        
+        try {
+            obj = em.find(User.class, id);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return obj;
+        
+    }
+    
+    public void save(User obj) {
+        
+        try {
+            em.getTransaction().begin();
+            em.merge(obj);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    public void delete(User obj) {
+        
+        try {
+            em.getTransaction().begin();
+            em.remove(obj);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    public List<User> findAll() {
+        
+        List<User> obj = null;
+        
+        try {
+            obj = em.createQuery("select t from User t").getResultList();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return obj;
+        
+    }
     
 }
