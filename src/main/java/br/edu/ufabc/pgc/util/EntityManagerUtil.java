c609105cb1f839a1984a3cc4dff04e980d62e37b
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-05-30 09:23:31 -0300

    Adicionando o persistence.xml

--- src/main/java/br/edu/ufabc/pgc/util/EntityManagerUtil.java
+++ src/main/java/br/edu/ufabc/pgc/util/EntityManagerUtil.java
@@ -9,6 +9,32 @@ package br.edu.ufabc.pgc.util;
  *
  * @author isaac
  */
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EntityManagerUtil {
+    
+    private static EntityManagerFactory emf = null;
+    private static EntityManager em = null;
+    
+    public static EntityManager getEntityManager() {
+        
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("pgc");
+        }
+        
+        if (em == null) {
+            em = emf.createEntityManager();
+        }
+        
+        return em;
+        
+    }
+    
+    public static void closeConnection() {
+        em.close();
+    }
     
 }
